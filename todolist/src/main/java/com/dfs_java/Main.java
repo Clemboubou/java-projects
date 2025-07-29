94cf1d92c42024cb3b99da0d622e901fa4dac918
Clement <ton-email@example.com>
2025-07-29 11:05:18 +0200

    todolist

--- todolist/src/main/java/com/dfs_java/Main.java
+++ todolist/src/main/java/com/dfs_java/Main.java
@@ -1,13 +1,20 @@
 package com.dfs_java;
-
 import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         Scanner scanner = new Scanner(System.in);
-        String name = scanner.nextLine();
-        System.out.println("Hello " + name);
+        String[] jeux = new String[5];
+        System.out.println("Quelles sont vos jeux vidéos favoris ?");
+        for (int i = 0; i < 5; i++) {
+            System.out.print((i+1) + ". ");
+            jeux[i] = scanner.nextLine();
+        }
+        System.out.println("\nVos jeux vidéos favoris sont :");
+        for (int i = 0; i < 5; i++) {
+            System.out.println((i+1) + ". " + jeux[i]);
+        }
         scanner.close();
     }
 }
